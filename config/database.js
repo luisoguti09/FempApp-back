7bd94eeda32531b053303217673f5dacffe0be87
Luis Gutierrez <luisoguti09@gmail.com>
2026-05-02 20:32:23 -0300

    database conected

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('fempatin', 'root', '', {
+/*const sequelize = new Sequelize('fempatin', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
+});*/
+const sequelize = new Sequelize('railway', 'root', 'CAtFaezrXbDgIiaknlKQZhCYaoMhKHjK', {
+  host: 'tramway.proxy.rlwy.net',
+  port: 16455,
+  dialect: 'mysql',
+  logging: false, // Desactiva los logs de Sequelize
 });
 
 module.exports = sequelize;
+//mysql://root:CAtFaezrXbDgIiaknlKQZhCYaoMhKHjK@tramway.proxy.rlwy.net:16455/railway
