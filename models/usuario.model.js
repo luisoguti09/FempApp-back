5464f3130f02d6a608fa8276f6cdb617e3366005
Luis Gutierrez <luisoguti09@gmail.com>
2026-05-03 00:06:31 -0300

    rol id added

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -40,6 +40,14 @@ const Usuario = sequelize.define('Usuario', {
     values: ['administrador', 'tecnico', 'deportista'],
     allowNull: false
   },
+  rolId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Roles',
+      key: 'id'
+    }
+  },
   fotoPerfil: {
     type: DataTypes.STRING,
     allowNull: true
